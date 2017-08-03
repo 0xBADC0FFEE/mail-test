7f5c88a89775b2ef5d3be63f9c348f5b808d9d2c
Artem Burtsev <vawerv@gmail.com>
2017-08-03 13:31:50 +0300

    Implemented strings count

--- main.go
+++ main.go
@@ -2,23 +2,95 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/0xBADC0FFEE/mail-test/pkg/manager"
+	"github.com/0xBADC0FFEE/mail-test/pkg/sources"
 )
 
 func main() {
-	var typeFlag InputType
+	var (
+		typeFlag   InputType
+		stringFlag string
+		kFlag      int
+	)
 
 	flag.Var(&typeFlag, "type", "Source type")
+	flag.StringVar(&stringFlag, "string", "Go", "String to count")
+	flag.IntVar(&kFlag, "k", 5, "Maximum workers")
 	flag.Parse()
-
-	println(typeFlag)
 
 	inputReader := bufio.NewScanner(os.Stdin)
 
-	for inputReader.Scan() {
-		path := inputReader.Text()
-		log.Print(path)
+	jobsManager := manager.New(kFlag)
+	jobsManager.Wake()
+
+	results := make(chan Result)
+
+	jobs := 0
+	completed := 0
+
+	go func() {
+
+		for inputReader.Scan() {
+			jobs++
+			path := inputReader.Text()
+
+			jobsManager.Add(func() {
+				var source sources.Source
+
+				switch typeFlag {
+				case URL:
+					source = sources.NewUrlReader(path)
+				case FILE:
+					source = sources.NewFileReader(path)
+				}
+
+				data, err := source.Get()
+				if err != nil {
+					println(fmt.Sprintf("Error getting data from %s: %v", path, err))
+				}
+
+				results <- Result{
+					Source: path,
+					Count:  bytes.Count(data, []byte(stringFlag)),
+					Error:  err,
+				}
+
+				completed++
+
+				if completed >= jobs {
+					close(results)
+				}
+
+			})
+
+		}
+
+	}()
+
+	total := 0
+
+	for result := range results {
+
+		if result.Error != nil {
+			log.Printf("Failed counting strings for %s: %v", result.Source, result.Error)
+			continue
+		}
+
+		log.Printf("Count for %s: %d", result.Source, result.Count)
+		total += result.Count
 	}
+
+	log.Printf("Total: %d", total)
+}
+
+type Result struct {
+	Source string
+	Count  int
+	Error  error
 }
